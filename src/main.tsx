1d27edf0bda87cfd84363f257f7629273d20c914
Keisuke Imai <Keisuke.Imai3@ibm.com>
2023-05-29 09:13:56 +0900

    StrictModeを解除

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// StrictModeを有効にした場合、
+// useEffectによる初期表示処理等が重複して実行されてしまい、
+// 想定外の動作になる可能性があるた解除する
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
