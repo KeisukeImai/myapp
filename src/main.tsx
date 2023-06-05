ae1816ef0b767abf567d88819624cbfc87bfb046
Keisuke Imai <Keisuke.Imai3@ibm.com>
2023-06-06 08:09:00 +0900

    リファクタリング

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@ if (import.meta.env.VITE_CLIENT_ONLY) {
 // StrictModeを有効にした場合、
 // useEffectによる初期表示処理等が重複して実行されてしまい、
 // 想定外の動作になる可能性があるた解除する
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
   <App />
